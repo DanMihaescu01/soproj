0276528f61a086a3ff8752ed94881a8cd92f9b6b
DanMihaescu01 <127722401+DanMihaescu01@users.noreply.github.com>
2023-11-21 21:41:18 +0200

    Update s7.c

--- s7.c
+++ s7.c
@@ -6,32 +6,31 @@
 #include <dirent.h>
 #include <string.h>
 #include <time.h>
-#include <limits.h>
-#include <sys/types.h>
 
 void process_file(const char *file, int fout) {
     int fd = open(file, O_RDONLY);
-    if(fd == -1) {
+    if (fd == -1) {
         perror("error open");
         return;
     }
 
     int width, height;
-    lseek(fd, 18, SEEK_SET);
-    if(read(fd, &width, sizeof(int)) != sizeof(int) || read(fd, &height, sizeof(int)) != sizeof(int)) {
+    if (lseek(fd, 18, SEEK_SET) == -1 ||
+        read(fd, &width, sizeof(int)) != sizeof(int) ||
+        read(fd, &height, sizeof(int)) != sizeof(int)) {
         perror("error read");
         close(fd);
         return;
     }
+    close(fd);
 
     struct stat info;
-    if(stat(file, &info) == -1) {
+    if (stat(file, &info) == -1) {
         perror("error stat");
-        close(fd);
         return;
     }
 
-    char buffer[256];
+    char buffer[512];
     if (strstr(file, ".bmp") != NULL) {
         sprintf(buffer, "nume fisier: %s\ninaltime: %d\nlatime: %d\ndimensiune: %ld\n", file, height, width, info.st_size);
     } else {
@@ -41,10 +40,10 @@ void process_file(const char *file, int fout) {
 
     struct tm *lastm;
     lastm = localtime(&info.st_mtime);
-    sprintf(buffer, "identificatorul utilizatorului: %d\ntimpul ultimei modificari: %d.%d.%d\ncontorul de legaturi: %d\n", info.st_uid, lastm->tm_mday, (lastm->tm_mon + 1), (lastm->tm_year + 1900), info.st_nlink);
+    sprintf(buffer, "identificatorul utilizatorului: %d\ntimpul ultimei modificari: %d.%d.%d\ncontorul de legaturi: %lu\n", info.st_uid, lastm->tm_mday, (lastm->tm_mon + 1), (lastm->tm_year + 1900), info.st_nlink);
     write(fout, buffer, strlen(buffer));
 
-    sprintf(buffer, "drepturi de acces user: ");
+      sprintf(buffer, "drepturi de acces user: ");
     if(info.st_mode & S_IRUSR) strcat(buffer,"R");
     else strcat(buffer,"-");
   
@@ -85,84 +84,83 @@ void process_file(const char *file, int fout) {
 
 void process_symlink(const char *link, const char *file, int fout) {
     struct stat link_stat, target_stat;
-    if(lstat(link, &link_stat) == -1 || stat(file, &target_stat) == -1) {
+    if (lstat(link, &link_stat) == -1 || stat(file, &target_stat) == -1) {
         perror("error stat");
         return;
     }
 
     char buffer[256];
     sprintf(buffer, "nume legatura: %s\ndimensiune legatura: %ld\ndimensiune fisier: %ld\ndrepturi de acces user legatura: ", link, link_stat.st_size, target_stat.st_size);
-    if(link_stat.st_mode & S_IRUSR) strcat(buffer,"R");
-    else strcat(buffer,"-");
-  
-    if(link_stat.st_mode & S_IWUSR) strcat(buffer,"W");
-    else strcat(buffer,"-");
-  
-    if(link_stat.st_mode & S_IXUSR) strcat(buffer,"X");
-    else strcat(buffer,"-");
-    strcat(buffer,"\n");
+    if (link_stat.st_mode & S_IRUSR) strcat(buffer, "R");
+    else strcat(buffer, "-");
+
+    if (link_stat.st_mode & S_IWUSR) strcat(buffer, "W");
+    else strcat(buffer, "-");
+
+    if (link_stat.st_mode & S_IXUSR) strcat(buffer, "X");
+    else strcat(buffer, "-");
+    strcat(buffer, "\n");
     write(fout, buffer, strlen(buffer));
 }
 
 void process_directory(const char *dir, int fout) {
-    char buffer[256];
     DIR *dp = opendir(dir);
-    if(!dp) {
+    if (!dp) {
         perror("error opendir");
         return;
     }
 
     struct dirent *entry;
-    while((entry = readdir(dp)) != NULL) {
-        if(strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
-            char file_path[256];
-            sprintf(file_path, "%s/%s", dir, entry->d_name);
+    while ((entry = readdir(dp)) != NULL) {
+        if (strcmp(entry->d_name, ".") != 0 && strcmp(entry->d_name, "..") != 0) {
+            char file_path[512];
+            snprintf(file_path, sizeof(file_path), "%s/%s", dir, entry->d_name);
 
             struct stat file_stat;
-            if(lstat(file_path, &file_stat) == -1) {
+            if (lstat(file_path, &file_stat) == -1) {
                 perror("error stat");
                 continue;
             }
 
-            if(S_ISREG(file_stat.st_mode)) {
-                if(strstr(entry->d_name, ".bmp") != NULL) {
+            if (S_ISREG(file_stat.st_mode)) {
+                if (strstr(entry->d_name, ".bmp") != NULL) {
                     process_file(file_path, fout);
                 } else {
-                    sprintf(buffer, "nume fisier: %s\ndimensiune: %ld\n", file_path, file_stat.st_size);
-                    write(fout, buffer, strlen(buffer));
+                    process_file(file_path, fout);
                 }
-            } else if(S_ISLNK(file_stat.st_mode)) {
-                char target_path[PATH_MAX];
+            } else if (S_ISLNK(file_stat.st_mode)) {
+                char target_path[512];
                 long long len = readlink(file_path, target_path, sizeof(target_path) - 1);
-                if(len != -1) {
+                if (len != -1) {
                     target_path[len] = '\0';
                     process_symlink(file_path, target_path, fout);
                 }
-                
-            } 
-            else if(S_ISDIR(file_stat.st_mode)) {
-                sprintf(buffer, "nume director: %s\nidentificatorul utilizatorului: %d\ndrepturi de acces user: RWX\ndrepturi de acces grup: R--\ndrepturi de acces altii: ---\n", file_path, file_stat.st_uid);
+
+            } else if (S_ISDIR(file_stat.st_mode)) {
+                char buffer[1024]; // Mărimea buffer-ului a fost mărită la 1024 de octeți
+                sprintf(buffer, "nume director: %s\nidentificatorul utilizatorului: %d\n", file_path, file_stat.st_uid);
                 write(fout, buffer, strlen(buffer));
-            }
+            } // Am adăugat acolada lipsă pentru blocul else if
         }
     }
     closedir(dp);
-}
+} // Am adăugat acolada lipsă pentru funcția process_directory
+
 
 int main(int argc, char *argv[]) {
-    if(argc != 2) {
+    if (argc != 2) {
         printf("Usage: %s <director_intrare>\n", argv[0]);
         return 1;
     }
 
-    int fout = creat("statistica.txt", S_IWUSR);
-    if(fout == -1) {
+    int fout = creat("statistica.txt", S_IWUSR | S_IRUSR); // Deschide fișierul cu permisiuni de scriere și citire pentru utilizator
+    if (fout == -1) {
         perror("error open");
         return 1;
     }
 
     struct stat dir_stat;
-    if(stat(argv[1], &dir_stat) == -1 || !S_ISDIR(dir_stat.st_mode)) {
+    if (stat(argv[1], &dir_stat) == -1 || !S_ISDIR(dir_stat.st_mode)) {
         printf("Argumentul nu este un director valid.\n");
         close(fout);
         return 1;
@@ -170,7 +168,7 @@ int main(int argc, char *argv[]) {
 
     process_directory(argv[1], fout);
 
-    if(close(fout) == -1) {
+    if (close(fout) == -1) {
         perror("error close");
         return 1;
     }
